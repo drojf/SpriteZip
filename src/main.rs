c39bdddcb7dd07247420cd805dcc00d78d3a1dfc
drojf <drojfjord@gmail.com>
2018-06-11 09:29:50 +1000

    workingish version

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use core::slice::Iter;
 use time::PreciseTime;
 
 
-use image::{GenericImage, imageops};
+use image::{RgbaImage , ImageBuffer, GenericImage, imageops};
 
 use walkdir::WalkDir;
 
@@ -101,10 +101,12 @@ fn SubtractTwoImages(img1_dyn : &image::DynamicImage, img2_dyn : &image::Dynamic
 }
 
 fn main() {
+    let canvas_width = 3000;
+    let canvas_height = 3000;
+
     let debug_mode = true;
 
-    let file_name_no_ext = "compressed_image";
-    let f = File::create([file_name_no_ext, ".brotli"].concat()).expect("Cannot create file");
+    let f = File::create(["compressed_images", ".brotli"].concat()).expect("Cannot create file");
 
     let mut compressor = brotli::CompressorWriter::new(
     f,
@@ -112,8 +114,10 @@ fn main() {
     9,//11, //9 seems to be a good tradeoff...changing q doesn't seem to make much diff though?
     22);
 
-    let mut img1_dyn_some = None;
-    let mut img2_dyn_some = None;
+    let mut canvas = RgbaImage::new(canvas_width, canvas_height);
+
+    //let mut img1_dyn_some = None;
+    //let mut img2_dyn_some = None;
 
     println!("Begin scanning for images");
 
@@ -121,9 +125,11 @@ fn main() {
 
     let test_iter = WalkDir::new("input_images");
     let mut count = 0;
-    for entry in test_iter {
-
+    for entry in test_iter
+    {
         let ent = entry.unwrap();
+
+        let file_name_no_ext = ent.path().file_stem().unwrap().to_str().unwrap();//ent.path();
 
         if ent.file_type().is_dir() {
             continue;
@@ -133,15 +139,49 @@ fn main() {
         {
             println!("Scan Image1");
             println!("first_item: {}", ent.path().display());
-            img1_dyn_some = Some(image::open(ent.path()).unwrap());
-            //let mut img1_dyn = image::open("1.png").unwrap();
-            //img1 = Some(img1_dyn.as_mut_rgba8().unwrap());
+
+            let img_dyn = image::open(ent.path()).unwrap();
+            let img = img_dyn.as_rgba8().unwrap();
+            for (x, y, pixel) in img.enumerate_pixels()
+            {
+                //for now just copy in the pixels
+                canvas.put_pixel(x,y, *pixel);
+            }
         }
         else
         {
             println!("Scan Image2");
             println!("{}", ent.path().display());
-            img2_dyn_some = Some(image::open(ent.path()).unwrap());
+
+            //for all ohter images, subtract image, then copy over image
+            let img_dyn = image::open(ent.path()).unwrap();
+            let img = img_dyn.as_rgba8().unwrap();
+            for (x, y, pixel) in img.enumerate_pixels()
+            {
+                let mut canvas_pixel = canvas.get_pixel_mut(x,y);
+
+                //TODO: use actal alpha value
+                let new_pixel = [
+                    pixel[0] - canvas_pixel[0],
+                    pixel[1] - canvas_pixel[1],
+                    pixel[2] - canvas_pixel[2],
+                    255];
+
+                *canvas_pixel = image::Rgba(new_pixel);
+            }
+
+            let save_path = [file_name_no_ext, ".png"].concat();
+            println!("Saving image to: {}", save_path);
+            canvas.save(save_path).unwrap();
+
+            //clear canvas (there must be a better way to do this?
+            canvas = RgbaImage::new(canvas_width, canvas_height);
+
+            //copy the just subtracted image onto canvas
+            canvas.copy_from(img, 0, 0);
+
+
+            /*img2_dyn_some = Some(image::open(ent.path()).unwrap());
             //let mut img2_dyn = image::open("2.png").unwrap();
             //img2 = Some(img2_dyn.as_mut_rgba8().unwrap());
             {
@@ -169,9 +209,11 @@ fn main() {
                 {
                     pixel[3] = 255;
                 }
-            }
+            }*/
 
         }
+
+        count += 1;
     }
 
     let file_name_no_ext = "compressed_image";
