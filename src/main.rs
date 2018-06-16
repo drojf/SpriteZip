b7783ec4f6d5b5082e78d4e7045391399607e219
drojf <drojfjord@gmail.com>
2018-06-16 10:57:43 +1000

    Refactor main into functions

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,33 @@ use walkdir::WalkDir;
 
 // TODO: crop diff'd images  so that not so much data needs to be compressed?
 
-/*fn SubtractTwoImages(img1_dyn : &image::DynamicImage, img2_dyn : &image::DynamicImage, debug_mode : bool)
+fn subtract_image_from_canvas(canvas: &mut image::RgbaImage, img : &image::RgbaImage, debug_mode : bool)
 {
-}*/
+    for (x, y, pixel) in img.enumerate_pixels()
+    {
+        let mut canvas_pixel = canvas.get_pixel_mut(x,y);
+
+        //TODO: disable debug mode to use alpha value
+        //must specify u8 to ensure wrapping occurs
+        let new_pixel : [u8; 4] = [
+            pixel[0].wrapping_sub(canvas_pixel[0]),
+            pixel[1].wrapping_sub(canvas_pixel[1]),
+            pixel[2].wrapping_sub(canvas_pixel[2]),
+            if debug_mode {255} else {pixel[3].wrapping_sub(canvas_pixel[3])},
+        ];
+
+        *canvas_pixel = image::Rgba(new_pixel);
+    }
+}
+
+fn save_brotli_image<T>(compressor : &mut brotli::CompressorWriter<T>, canvas_as_raw : &Vec<u8>)
+where T: std::io::Write
+{
+    let brotli_start = PreciseTime::now();
+    compressor.write(canvas_as_raw).unwrap();
+    let brotli_end = PreciseTime::now();
+    println!("Brotli compression took {} seconds", brotli_start.to(brotli_end));
+}
 
 fn main() {
     let canvas_width = 3000;
@@ -59,29 +83,13 @@ fn main() {
         }
 
         let save_path = [file_name_no_ext, ".png"].concat();
+        println!("{}", ent.path().display());
         println!("Will save image to: {}", save_path);
 
         let img_dyn = image::open(ent.path()).unwrap();
         let img = img_dyn.as_rgba8().unwrap();
 
-        println!("{}", ent.path().display());
-
-        //subtract the image
-        for (x, y, pixel) in img.enumerate_pixels()
-        {
-            let mut canvas_pixel = canvas.get_pixel_mut(x,y);
-
-            //TODO: disable debug mode to use alpha value
-            //must specify u8 to ensure wrapping occurs
-            let new_pixel : [u8; 4] = [
-                pixel[0].wrapping_sub(canvas_pixel[0]),
-                pixel[1].wrapping_sub(canvas_pixel[1]),
-                pixel[2].wrapping_sub(canvas_pixel[2]),
-                if debug_mode {255} else {pixel[3].wrapping_sub(canvas_pixel[3])},
-            ];
-
-            *canvas_pixel = image::Rgba(new_pixel);
-        }
+        subtract_image_from_canvas(&mut canvas, &img, debug_mode);
 
         //save diff image as png for debugging reasons
         canvas.save(save_path).unwrap();
@@ -89,19 +97,13 @@ fn main() {
         // Compress the the diff image (or 'normal' image for first image)
         // NOTE: the below 'into_raw()' causes a move, so the canvas cannot be used anymore
         // However subsequent RgbaImage::new assigns a new value to the canvas each iteration
-        {
-            let canvas_as_raw = canvas.into_raw();
-
-            let brotli_start = PreciseTime::now();
-            compressor.write(&canvas_as_raw).unwrap();
-            let brotli_end = PreciseTime::now();
-            println!("Brotli compression took {} seconds", brotli_start.to(brotli_end));
-        }
+        let canvas_as_raw = canvas.into_raw();
+        save_brotli_image(&mut compressor, &canvas_as_raw);
 
         //clear canvas (there must be a better way to do this?
         canvas = RgbaImage::new(canvas_width, canvas_height);
 
-        //copy the originalimage onto canvas for next iteration
+        //copy the original image onto canvas for next iteration
         canvas.copy_from(img, 0, 0);
 
 
