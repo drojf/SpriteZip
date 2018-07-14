112920f4c1608a332d97f4ec035f5111e05c26b4
drojf <drojfjord@gmail.com>
2018-07-14 10:48:39 +1000

    Add kinda working version of byte counter

--- src/byte_counter.rs
+++ src/byte_counter.rs
@@ -1,18 +1,25 @@
 /// From https://stackoverflow.com/questions/42187591/how-to-keep-track-of-how-many-bytes-written-when-using-stdiowrite
+//let mut compressed_bitmap_wrapper = ByteCounter::new(&mut compressed_bitmap_data_vector , 1_000_000);
+//let mut bitmap_compressor = brotli::CompressorWriter::new(compressed_bitmap_wrapper, BROTLI_BUFFER_SIZE, brotli_quality, brotli_window);
+
 use std::io::{self, Write, Seek, SeekFrom};
 
 pub struct ByteCounter<W> {
     inner: W,
     count: usize,
+    bytes_seen: usize,
+    print_size: usize,
 }
 
 impl<W> ByteCounter<W>
     where W: Write
 {
-    pub fn new(inner: W) -> Self {
+    pub fn new(inner: W, print_size : usize) -> Self {
         ByteCounter {
             inner: inner,
             count: 0,
+            bytes_seen: 0,
+            print_size,
         }
     }
 
@@ -28,24 +35,21 @@ impl<W> ByteCounter<W>
 impl<W> Write for ByteCounter<W>
     where W: Write
 {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let res = self.inner.write(buf);
         if let Ok(size) = res {
+            self.bytes_seen += size;
+            if self.bytes_seen > self.print_size {
+                self.bytes_seen = 0;
+                println!("Mem: {}", pretty_print_bytes(self.count as f64))
+            }
+
             self.count += size
         }
         res
     }
 
-    fn flush(&mut self) -> io::Result<()> {
+    fn flush(&mut self) -> std::io::Result<()> {
         self.inner.flush()
-    }
-}
-
-impl<W> Seek for ByteCounter<W>
-    where W: Seek
-{
-    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64>
-    {
-        self.inner.seek(pos)
     }
 }
